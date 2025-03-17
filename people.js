c167d6c53ff0478b6446cb8962bec42967128732
andreasdraxl <andreas_draxl_@live.de>
2025-03-17 16:41:13 +0100

    add css to peoples card

--- people.js
+++ people.js
@@ -18,38 +18,38 @@ function closeModal(id) {
     }
 }
 
-// "New User" Button - Öffnet das Modal
-const newUserBtn = document.getElementById("new-User-btn");
-if (newUserBtn) {
-    newUserBtn.addEventListener("click", () => showModal("new-User-modal"));
+// "New user" Button - Öffnet das Modal
+const newuserBtn = document.getElementById("new-user-btn");
+if (newuserBtn) {
+    newuserBtn.addEventListener("click", () => showModal("new-user-modal"));
 } else {
-    console.warn("No new User button found.");
+    console.warn("No new user button found.");
 }
 
 // "Cancel" Button - Schließt das Modal
 const cancelBtn = document.querySelector(".btn-secondary"); 
 if (cancelBtn) {
-    cancelBtn.addEventListener("click", () => closeModal("new-User-modal"));
+    cancelBtn.addEventListener("click", () => closeModal("new-user-modal"));
 } else {
     console.warn("No cancel button found.");
 }
 
 // Formular-Submit verhindern und Daten loggen
-const UserForm = document.getElementById("new-User-form");
-if (UserForm) {
-    UserForm.addEventListener("submit", (e) => {
+const userForm = document.getElementById("new-user-form");
+if (userForm) {
+    userForm.addEventListener("submit", (e) => {
         e.preventDefault();
-        const formData = new FormData(UserForm)
-        const User = {
+        const formData = new FormData(userForm)
+        const user = {
             name: formData.get("name"),
             discription: formData.get("discription"),
             role: formData.get("role"),
             status: formData.get("status"),
             date: formData.get("date")
         }
-        console.log(User);
-        closeModal("new-User-modal"); // Schließt das Modal nach dem Submit
+        console.log(user);
+        closeModal("new-user-modal"); // Schließt das Modal nach dem Submit
     });
 } else {
-    console.warn("No new User form found. Check the ID!");
+    console.warn("No new user form found. Check the ID!");
 }
